7f6fb1bd1e9f15f9b9de95d4fe90a0b81450743f
Markus Böck <markus.boeck02@gmail.com>
2024-05-21 13:46:47 +0100

    Enable FPU by default
    This matches the behavior of the snitch cluster and allows us to use the startup script of the snitch runtime without changes

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -525,7 +525,7 @@ reg_t mstatus_csr_t::compute_mstatus_initial_value() const noexcept {
          | (proc->extension_enabled_const('U') && (proc->get_const_xlen() != 32) ? set_field((reg_t)0, MSTATUS_UXL, xlen_to_uxl(proc->get_const_xlen())) : 0)
          | (proc->extension_enabled_const('S') && (proc->get_const_xlen() != 32) ? set_field((reg_t)0, MSTATUS_SXL, xlen_to_uxl(proc->get_const_xlen())) : 0)
          | (proc->get_mmu()->is_target_big_endian() ? big_endian_bits : 0)
-         | 0;  // initial value for mstatus
+         | MSTATUS_FS;  // initial value for mstatus
 }
 
 // implement class mnstatus_csr_t
